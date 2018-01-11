9dbff2097bb1fe8193fac8d42a6a3695934a5b70
wes lawler <wes.lawler2@gmail.com>
2018-01-10 18:53:30 -0600

    new compenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Hw2Component } from './hw2/hw2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Hw2Component
   ],
   imports: [
     BrowserModule,
